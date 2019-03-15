421e140b102294aa3d3c400321a8c157bfcfad11
Guillaume <guillaume.huet@hei.yncrea.fr>
2019-03-15 12:05:40 +0100

    Joystick mis en place !

--- app/src/main/java/hei/balamba_boca_campion_delloye_duclos_huet/r2_d2/ControlActivity.java
+++ app/src/main/java/hei/balamba_boca_campion_delloye_duclos_huet/r2_d2/ControlActivity.java
@@ -1,13 +1,35 @@
 package hei.balamba_boca_campion_delloye_duclos_huet.r2_d2;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class ControlActivity extends AppCompatActivity {
+
+    private TextView mTextViewAngleLeft;
+    private TextView mTextViewStrengthLeft;
+
+    private TextView mTextViewAngleRight;
+    private TextView mTextViewStrengthRight;
+    private TextView mTextViewCoordinateRight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+        JoystickView joystick = findViewById(R.id.joystickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                // do whatever you want
+                Log.d("Guigui",String.format("angle : %d force : %d",angle,strength));
+            }
+        });
+
     }
 }
+
