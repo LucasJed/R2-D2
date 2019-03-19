84529c56280a621f75c9c31f66113d5405363f26
Guillaume <guillaume.huet@hei.yncrea.fr>
2019-03-19 10:02:29 +0100

    Commande Control joystick : right/left/forward/backward !

--- app/src/main/java/hei/balamba_boca_campion_delloye_duclos_huet/r2_d2/ControlActivity.java
+++ app/src/main/java/hei/balamba_boca_campion_delloye_duclos_huet/r2_d2/ControlActivity.java
@@ -1,6 +1,8 @@
 package hei.balamba_boca_campion_delloye_duclos_huet.r2_d2;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,8 @@ public class ControlActivity extends AppCompatActivity {
     private TextView mTextViewCoordinateRight;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,19 @@ public class ControlActivity extends AppCompatActivity {
             @Override
             public void onMove(int angle, int strength) {
                 // do whatever you want
-                Log.d("Guigui",String.format("angle : %d force : %d",angle,strength));
+                if (46 < angle && angle < 136) {
+                    Log.d("R2-D2", String.format("angle : %d force : %d", angle, strength));
+                    Log.d("R2-D2", String.format("Forward"));
+                } else if (136 < angle && angle < 225) {
+                    Log.d("R2-D2", String.format("angle : %d force : %d", angle, strength));
+                    Log.d("R2-D2", String.format("Left"));
+                } else if (226 < angle && angle < 315) {
+                    Log.d("R2-D2", String.format("angle : %d force : %d", angle, strength));
+                    Log.d("R2-D2", String.format("Backward"));
+                } else {
+                    Log.d("R2-D2", String.format("angle : %d force : %d", angle, strength));
+                    Log.d("R2-D2", String.format("Right"));
+                }
             }
         });
 
--- app/src/main/java/hei/balamba_boca_campion_delloye_duclos_huet/r2_d2/LookActivity.java
+++ app/src/main/java/hei/balamba_boca_campion_delloye_duclos_huet/r2_d2/LookActivity.java
@@ -2,12 +2,38 @@ package hei.balamba_boca_campion_delloye_duclos_huet.r2_d2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
 
 public class LookActivity extends AppCompatActivity {
 
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_look);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("10.127.0.119");
+
+        Log.i(TAG, "Entering onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.clearCache(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://10.0.2.2:8080/SampleWebServer/Welcome.html");
+        Log.i(TAG, "Exiting onCreate");
+
     }
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+
+
+    }
+*/
+
 }
